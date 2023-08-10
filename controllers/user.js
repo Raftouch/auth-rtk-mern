146786988132b17828387ad099e345daa346db89
Raftouch <rafanelkin@gmail.com>
2023-08-10 13:50:36 +0200

    :bricks: created routes

--- controllers/user.js
+++ controllers/user.js
@@ -3,10 +3,31 @@ const asyncHandler = require('express-async-handler')
 // route     POST /api/users/auth
 // @access   Public
 const auth = asyncHandler(async (req, res) => {
-  //   res.status(401)
-  //   throw new Error('Something went wrong')
-
   res.status(200).json({ message: 'Auth User' })
 })
 
-module.exports = { auth }
+// route     POST /api/users
+// @access   Public
+const register = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Register User' })
+})
+
+// route     POST /api/users/logout
+// @access   Public
+const logout = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout User' })
+})
+
+// route     GET /api/users/profile
+// @access   Private
+const getProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User Profile' })
+})
+
+// route     PUT /api/users/profile
+// @access   Private
+const updateProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update User Profile' })
+})
+
+module.exports = { auth, register, logout, getProfile, updateProfile }
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const { auth } = require('../controllers/user')
+const {
+  auth,
+  register,
+  logout,
+  getProfile,
+  updateProfile,
+} = require('../controllers/user')
 
 router.post('/auth', auth)
+router.post('/', register)
+router.post('/logout', logout)
+router.route('/profile').get(getProfile).put(updateProfile)
 
 module.exports = router
