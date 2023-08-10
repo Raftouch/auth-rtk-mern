fe0afefc687abb034be50c1583d92d8230399b5f
Raftouch <rafanelkin@gmail.com>
2023-08-10 13:17:35 +0200

    :goal_net: customized error handler

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const { notFound, errorHandler } = require('./middleware/error')
 const port = process.env.PORT || 3000
 
 app.use('/api/users', require('./routes/user'))
+app.use(notFound, errorHandler)
 
 app.get('/', (req, res) => {
   res.send('App works')
--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,9 @@ const asyncHandler = require('express-async-handler')
 // route     POST /api/users/auth
 // @access   Public
 const auth = asyncHandler(async (req, res) => {
+  //   res.status(401)
+  //   throw new Error('Something went wrong')
+
   res.status(200).json({ message: 'Auth User' })
 })
 
