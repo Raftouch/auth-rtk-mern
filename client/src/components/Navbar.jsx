236585da518aceadf6a9b350807d3f16e525ab4b
Raftouch <rafanelkin@gmail.com>
2023-08-13 16:48:20 +0200

    :bento: updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Navbar() {
+  const { userInfo } = useSelector((state) => state.auth)
+
   return (
     <nav className="h-[80px] w-full flex justify-between items-center px-5 border-b-2">
       <NavLink to="/">
         <img src="" alt="" />
       </NavLink>
       <span className="flex gap-5">
-        <NavLink to="/register">Register</NavLink>
-        <NavLink to="/login">Login</NavLink>
+        {userInfo ? (
+          <>
+            <div className="text-green-600 uppercase">{userInfo.name}</div>
+            <NavLink to="/profile">Profile</NavLink>
+            <NavLink to="/logout">Logout</NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </>
+        )}
       </span>
     </nav>
   )
