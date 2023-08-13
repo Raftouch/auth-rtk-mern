f8a02f81376974157b7fb388cae3dc572566735d
Raftouch <rafanelkin@gmail.com>
2023-08-13 17:09:01 +0200

    :boom: logout functionality ready

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,27 @@
-import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { clearCredentials } from '../slices/auth'
+import { useLogoutMutation } from '../slices/usersApi'
 
 export default function Navbar() {
   const { userInfo } = useSelector((state) => state.auth)
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const [logout] = useLogoutMutation()
+
+  const logoutHandler = async () => {
+    try {
+      // to destroy a cookie
+      await logout().unwrap()
+      // to clear local storage
+      dispatch(clearCredentials())
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <nav className="h-[80px] w-full flex justify-between items-center px-5 border-b-2">
@@ -14,7 +33,9 @@ export default function Navbar() {
           <>
             <div className="text-green-600 uppercase">{userInfo.name}</div>
             <NavLink to="/profile">Profile</NavLink>
-            <NavLink to="/logout">Logout</NavLink>
+            <NavLink to="/logout" onClick={logoutHandler}>
+              Logout
+            </NavLink>
           </>
         ) : (
           <>
